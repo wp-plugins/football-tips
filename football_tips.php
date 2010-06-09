b434c1d0d35b7b7c92c6a469e20a1bf58ad47956
goalmu <goalmu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-09 11:06:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/football-tips/tags/0.02@250260 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ football_tips.php
@@ -0,0 +1,97 @@
+<?php
+/*
+Plugin Name: Football_Tips
+Plugin URI: http://www.goal.mu/wordpress-widget
+Description: Display the Latest Football_Tips and the best Odds
+Author: Nababsingh-Ramdharry/Goal.mu
+Version: 0.02
+Author URI: http://www.goal.mu/wordpress-widget
+
+*/
+
+
+
+  
+  function widget_Football_Tips_init() 
+	  {
+
+	  
+	  /* Your Function */
+	  function Football_Tips()
+	  {
+		  
+
+		  
+		  $URL = "http://server1.goal.mu/bookieodds.php";
+$a = file_get_contents("$URL");
+echo ($a);
+		  
+
+		  
+	  }
+	  
+
+
+	  
+	  function widget_Football_Tips($args) 
+	  {
+	  
+	  	  // Collect our widget's options, or define their defaults.
+		  $options = get_option('widget_Football_Tips');
+		  $title = empty($options['title']) ? __('Football_Tips') : $options['title'];
+			
+		  extract($args);
+		  echo $before_widget;
+		  echo $before_title;
+		  echo $title;
+		  echo $after_title;
+		  Football_Tips();
+		  echo $after_widget;
+	  }  
+	  
+	
+	  
+	  function widget_Football_Tips_control()
+	  {
+	  
+		// Collect our widget options.
+		$options = $newoptions = get_option('widget_Football_Tips');
+		
+		// This is for handing the control form submission.
+		if ( $_POST['widget_Football_Tips-submit'] ) 
+		{
+			// Clean up control form submission options
+			$newoptions['title'] = strip_tags(stripslashes($_POST['widget_Football_Tips-title']));
+		}
+				
+		// If original widget options do not match control form
+		// submission options, update them.
+		if ( $options != $newoptions ) 
+		{
+			$options = $newoptions;
+			update_option('widget_Football_Tips', $options);
+		}
+						
+		$title = attribute_escape($options['title']);
+
+		echo '<p><label for="Football_Tips-title">';
+		echo 'Title: <input style="width: 250px;" id="widget_Football_Tips-title" name="widget_Football_Tips-title" type="text" value="';
+		echo $title;
+		echo '" />';
+		echo '</label></p>';
+		echo '<input type="hidden" id="widget_Football_Tips-submit" name="widget_Football_Tips-submit" value="1" />';
+	  }
+	  
+	  
+	// This registers the widget.
+    register_sidebar_widget('Football_Tips', 'widget_Football_Tips');
+	
+	// This registers the (optional!) widget control form.
+    register_widget_control('Football_Tips', 'widget_Football_Tips_control');
+	
+  }
+    
+  add_action('plugins_loaded', 'widget_Football_Tips_init');
+
+?>
+	
---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== Football Tips ===
+
+Contributors: Nababsingh Ramdharry
+Tags: bet, tips, widget, football, odds, football tips, bet compare, online bookies, soccer, soccer tips, world cup tips, world cup 2010 tips, FIFA World Cup 2010 Tips
+Requires at least: 2.0.2
+Tested up to: 2.9.2
+Stable tag: 0.02
+
+== Description ==
+The Goal.mu Football Tips Wordpress Widget will display the latest football tips from 
+Goal.mu together with the latest best odds from various online bookies.Football Tips from
+the England, France, Spain, Italy, Germany and so on.Get the latest FIFA World Cup 2010 betting 
+tips together with the best odds.
+
+== Installation ==
+Here are some installation instructions:
+
+   *  Click the download link below to get the latest version
+   *  Drop the tips Widget folder into your wp-content/plugins folder
+   *  Go to your Administration:Plugins page and activate the Football Tips Widget.
+   *  Go to Presentation:Widgets
+   *  Move (drag and drop) the widget to where you would like it in your active sidebar
+   *  Click on the config button for the Football Tips.
+   *  Enter the title you want visitors to see on the sidebar
+== Frequently Asked Questions ==
+
+= Is this a widget plugin? =
+
+Yes, this is a Widget Plugin.
+
+== Screenshots ==
+1. This is the screenshot of how the widget will be.
+
+== Changelog ==
+= 0.01 =
+This is the first version
+= 0.02 =
+Removal of Bookies images.
+
+== Upgrade Notice ==
+= 0.02 =
+Upgrade to Football Tips 0.02
+= 0.01 =
+There's no new version to upgrade to!
